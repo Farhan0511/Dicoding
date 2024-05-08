dd303a96e0d6c84acf2ccf86fb8fe73562733182
Farhan0511 <programingsukses@gmail.com>
2024-05-08 10:01:40 +0700

    Menambahkan folder proyek Movie-Catalogue

---
+++ Movie Catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '49f87e2ab53076601eaab0e0bfe66291',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
