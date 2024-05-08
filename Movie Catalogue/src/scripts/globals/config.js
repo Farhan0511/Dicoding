89970d39ba72578fff470db45d45f1529223ef13
Farhan0511 <programingsukses@gmail.com>
2024-05-08 10:54:21 +0700

    menghapus folder movie catalogue

--- Movie Catalogue/src/scripts/globals/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  KEY: '49f87e2ab53076601eaab0e0bfe66291',
-  BASE_URL: 'https://api.themoviedb.org/3/',
-  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
-  DEFAULT_LANGUAGE: 'en-us',
-};
-
-export default CONFIG;
