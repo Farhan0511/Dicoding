1fd8da62a75c5b5d18a0f78ab617a8dfa6096b10
Farhan0511 <programingsukses@gmail.com>
2024-05-14 10:18:26 +0700

    menambahkan tombol like pada halaman detail

--- Movie-Catalogue/src/scripts/utils/sw-register.js
+++ Movie-Catalogue/src/scripts/utils/sw-register.js
@@ -1,12 +1,16 @@
+import { Workbox } from 'workbox-window';
+
 const swRegister = async () => {
   if ('serviceWorker' in navigator) {
     if (!('serviceWorker' in navigator)) {
       console.log('Service Worker not supported in the browser');
       return;
     }
-    console.log('Service worker not supported in this browser');
+
+    const wb = new Workbox('./sw.bundle.js');
 
     try {
+      await wb.register();
       await navigator.serviceWorker.register('./sw.bundle.js');
       console.log('Service worker registered');
     } catch (error) {
